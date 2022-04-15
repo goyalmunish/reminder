c6abe1c54efa7aa89f360a1755ee7134b327e9f6
Munish Goyal <munishapc@gmail.com>
2022-04-15 14:04:59 +1000

    Fix image build script

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -7,9 +7,10 @@ REMINDER_IMAGE=reminder
 REMINDER_CONTAINER=reminder
 TAG="${@:-latest}"
 
-# building and pushing default image
-echo "STEP-01: Build and tagging the default image"
+# building and tag image
+echo "STEP-01: Build the latest image"
 docker build -t goyalmunish/${REMINDER_IMAGE} -f Dockerfile ./
+docker tag goyalmunish/${REMINDER_IMAGE} goyalmunish/${REMINDER_IMAGE}:${TAG}
 echo "STEP-03: Push the images"
 docker push goyalmunish/${REMINDER_IMAGE}:latest
 docker push goyalmunish/${REMINDER_IMAGE}:${TAG}
