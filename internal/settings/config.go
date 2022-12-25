bfa4e3f9c42b351884086fddb5e28a215abbf1bf
Munish Goyal <munishapc@gmail.com>
2022-12-25 12:31:37 +1100

    Refactor to inject keys to logger instance (#149)

--- internal/settings/config.go
+++ internal/settings/config.go
@@ -2,12 +2,14 @@ package settings
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/goyalmunish/reminder/internal/appinfo"
 	"github.com/goyalmunish/reminder/pkg/calendar"
 	"github.com/goyalmunish/reminder/pkg/logger"
 	"github.com/goyalmunish/reminder/pkg/utils"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
 
 type Settings struct {
@@ -24,9 +26,16 @@ func DefaultSettings() *Settings {
 	}
 }
 
+func (s *Settings) String() string {
+	value, _ := yaml.Marshal(s)
+	return string(value)
+}
+
 func LoadConfig(ctx context.Context) (*Settings, error) {
+	appConfigPath := "./config/default.yaml"
 	settings := DefaultSettings()
-	viper.SetConfigFile("./config/default.yaml")
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(appConfigPath)
 	if err := viper.ReadInConfig(); err != nil {
 		utils.LogError(ctx, err)
 		return nil, err
@@ -35,5 +44,6 @@ func LoadConfig(ctx context.Context) (*Settings, error) {
 		utils.LogError(ctx, err)
 		return nil, err
 	}
+	logger.Info(ctx, fmt.Sprintf("Read the app config %q (on top of default values).", appConfigPath))
 	return settings, nil
 }
--- pkg/logger/options.go
+++ pkg/logger/options.go
@@ -9,11 +9,18 @@ type Options struct {
 	// DebugLevel 5
 	// TraceLevel 6
 	Level int8 `json:"level" yaml:"level" mapstructure:"level"`
+	// Fields is a list of log fields that the logger
+	// should become part of the log entry if available
+	// in the context.
+	LookupFields []string `json:"lookup_fields" yaml:"lookup_fields" mapstructure:"lookup_fields"`
 }
 
 func DefaultOptions() *Options {
 	return &Options{
-		// by default use debug level
+		// By default use debug level.
 		Level: 5,
+		// Value for the fields the logger should inject in the log
+		// entry (if they are not nil)
+		LookupFields: []string{"app", "run_id"},
 	}
 }
